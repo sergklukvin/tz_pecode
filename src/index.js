636963f44d193bacaaef48db1fa747822692f9ad
Serg Klukvin <sergklukvin@gmail.com>
2021-02-07 22:03:25 +0200

    improve

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import configureStore from './store/configureStore';
-
-
-const store = configureStore();
 
 ReactDOM.render(
   <React.Fragment>
